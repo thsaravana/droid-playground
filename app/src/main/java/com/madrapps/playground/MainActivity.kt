b33869dd9f94cb8a3fc46092821929c9d6f1ece4
thsaravana <th.saravana@gmail.com>
2021-09-19 14:07:20 +0530

    Version update

--- app/src/main/java/com/madrapps/playground/MainActivity.kt
+++ app/src/main/java/com/madrapps/playground/MainActivity.kt
@@ -1,10 +1,9 @@
-package com.madrapps.droid_playground
+package com.madrapps.playground
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
